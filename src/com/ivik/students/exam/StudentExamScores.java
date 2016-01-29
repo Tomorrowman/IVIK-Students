d47d5b9cc114f99dfac75ef0ce3f1b2d2392576f
Tomorrowman <J.W.Gesink@gmail.com>
2016-01-29 18:28:07 +0100

    Interface Update

--- src/com/ivik/students/exam/StudentExamScores.java
+++ src/com/ivik/students/exam/StudentExamScores.java
@@ -1,38 +1,35 @@
 package com.ivik.students.exam;
 
-import com.ivik.students.model.Student;
-import com.ivik.students.util.SortUtils;
-
 /**
  * Created by Eigenaar on 14-1-2016.
  */
-public class StudentExamScores {
-    private Student[]students;
-    public StudentExamScores(){
-        initExamScores();
-    }
+//public class StudentExamScores {
+    //private Student[]students;
+    //public StudentExamScores(){
+        //initExamScores();
+    //}
 
-    private void initExamScores() {
-        Student student1 = new Student("Ruud", 55.6);
-        Student student2 = new Student("Piet", 34.7);
-        Student student3 = new Student("Henk", 78.2);
-        Student student4 = new Student("Daan", 99.9);
-        Student student5 = new Student("Kees", 64.5);
+    //private void initExamScores() {
+        //Student student1 = new Student("Ruud", 55.6);
+        //Student student2 = new Student("Piet", 34.7);
+        //Student student3 = new Student("Henk", 78.2);
+        //Student student4 = new Student("Daan", 99.9);
+        //Student student5 = new Student("Kees", 64.5);
 
-        students= new Student []{student1, student2, student3, student4, student5};
-    }
-    public void reportStudentScores(){
-        SortUtils.sortStudents(students);
-        System.out.println("Name     "+"Score");
-        System.out.println("------   "+"------");
-        for (int i=0; i<=students.length-1; i++){
-            String name=students[i].getName();
-            double score=students[i].getScore();
-            System.out.format("%4s%9s%n", name, score);
-        }
-    }
+        //students= new Student []{student1, student2, student3, student4, student5};
+    //}
+    //public void reportStudentScores(){
+        //SortUtils.sortStudents(students);
+        //System.out.println("Name     "+"Score");
+        //System.out.println("------   "+"------");
+        //for (int i=0; i<=students.length-1; i++){
+            //String name=students[i].getName();
+            //double score=students[i].getScore();
+            //System.out.format("%4s%9s%n", name, score);
+        //}
+    //}
 
-    public static void main(String[] args) {
-        new StudentExamScores().reportStudentScores();
-    }
-}
+    //public static void main(String[] args) {
+        //new StudentExamScores().reportStudentScores();
+    //}
+//}
--- src/com/ivik/students/util/SortUtils.java
+++ src/com/ivik/students/util/SortUtils.java
@@ -6,12 +6,16 @@ import com.ivik.students.model.Student;
  * Created by Eigenaar on 13-1-2016.
  */
 public class SortUtils {
-    public static void sortStudents(Student[] students){
+    public static void sortStudents(Student[] students, StudentComparable compare){
         boolean sorted=false;
         while (!sorted){
             sorted=true;
             for (int i =0; i<students.length-1; i++) {
-                if (students[i].getScore() > students[i+1].getScore()){
+                int swap = compare.compareTo(students[i], students[i+1]);
+                if (swap == 0){
+                    continue;
+                }
+                else if (swap == -1){
                     Student tmp = students[i];
                     students[i] = students[i+1];
                     students[i+1] = tmp;
